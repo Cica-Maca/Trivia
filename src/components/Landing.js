934ebbc1c4a440c08d748f7da010a7074ba0296d
Cica-Maca <fcosic780@gmail.com>
2022-12-08 18:15:43 +0100

    Added ladning page

---
+++ src/components/Landing.js
@@ -0,0 +1,8 @@
+export default function Landing() {
+  return (
+    <div className='starting-page'>
+      <h1>Welcome to trivia questions</h1>
+      <button>Get trivia questions</button>
+    </div>
+  );
+}
