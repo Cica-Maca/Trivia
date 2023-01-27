75089111a561ae8e638f6a4ef72c6a546d8c5a4b
Cica-Maca <fcosic780@gmail.com>
2023-01-27 19:59:30 +0100

    Added the QuestionScore component, refactored the code from Info.js to QuestionScore component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,49 +1,36 @@
 import { useState } from 'react';
 import { parseEntities } from './utils';
+import QuestionScore from './QuestionScore';
 
 export default function Info({ getNewTrivia, userAnswers, questions }) {
   const [toggleAnswers, setToggleAnswers] = useState(false);
 
   const revisedQuestionsElements = questions.map((question, index) => {
     return (
-      <div className='question-Rev'>
-        <h1>{question.question}</h1>
-        <div className='answers-Rev'>
-          {question.answers.map((answer) => {
-            const markedCorrectAnswer =
-              answer === question.correctAnswer ? 'correct-answer' : '';
-            const markedUserAnswer =
-              answer === userAnswers[index].answer &&
-              !userAnswers[index].correct
-                ? 'incorrect-answer'
-                : '';
-
-            return (
-              <button className={`${markedCorrectAnswer} ${markedUserAnswer}`}>
-                {parseEntities(answer)}
-              </button>
-            );
-          })}
-        </div>
-      </div>
+      <QuestionScore
+        question={question}
+        userAnswers={userAnswers}
+        index={index}
+      />
     );
   });
 
-  console.log(questions);
-  console.log(userAnswers);
   const correctAnswersNum = userAnswers.filter(
     (answer) => answer.correct === true
   ).length;
 
+  const StartOrEndElement =
+    userAnswers.length === 0 ? (
+      <h1>Welcome to trivia.</h1>
+    ) : (
+      <h1>
+        You scored {correctAnswersNum}/{userAnswers.length} correct answers!
+      </h1>
+    );
+
   return (
     <div className='starting-page'>
-      {userAnswers.length === 0 ? (
-        <h1>Welcome to trivia.</h1>
-      ) : (
-        <h1>
-          You scored {correctAnswersNum}/{userAnswers.length} correct answers!
-        </h1>
-      )}
+      {StartOrEndElement}
       <button onClick={getNewTrivia}>Get trivia questions</button>
       {toggleAnswers && revisedQuestionsElements}
       {userAnswers.length > 0 && (
---
+++ src/components/QuestionScore.js
@@ -0,0 +1,25 @@
+import { parseEntities } from './utils';
+
+export default function QuestionScore({ question, userAnswers, index }) {
+  const checkedAnswersElements = question.answers.map((answer) => {
+    const markedCorrectAnswer =
+      answer === question.correctAnswer ? 'correct-answer' : '';
+    const markedUserAnswer =
+      answer === userAnswers[index].answer && !userAnswers[index].correct
+        ? 'incorrect-answer'
+        : '';
+
+    return (
+      <button className={`${markedCorrectAnswer} ${markedUserAnswer}`}>
+        {parseEntities(answer)}
+      </button>
+    );
+  });
+
+  return (
+    <div className='question-Rev'>
+      <h1>{question.question}</h1>
+      <div className='answers-Rev'>{checkedAnswersElements}</div>
+    </div>
+  );
+}
