40a78d9899eaa1c05b0393f357307dc349ee887b
Cica-Maca <fcosic780@gmail.com>
2023-01-31 20:31:19 +0100

    Cleaned the code in Info.js, made component for categories

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,6 +3,8 @@ import QuestionScore from './QuestionScore';
 
 import { SwitchTransition, CSSTransition } from 'react-transition-group';
 
+import Categories from './Categories';
+
 export default function Info({
   getNewTrivia,
   userAnswers,
@@ -29,7 +31,7 @@ export default function Info({
 
   const StartOrEndElement =
     userAnswers.length === 0 ? (
-      <h1>Welcome to trivia.</h1>
+      <h1>Trivia</h1>
     ) : (
       <h1>
         You scored {correctAnswersNum}/{userAnswers.length} correct answers!
@@ -54,37 +56,9 @@ export default function Info({
         <CSSTransition classNames='slideLeft' key={2} timeout={500}>
           <div className='info-page'>
             {StartOrEndElement}
-            <button onClick={getNewTrivia}>Get trivia questions</button>
-            <select
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-            >
-              <option value='any'>Any Category</option>
-              <option value='9'>General Knowledge</option>
-              <option value='10'>Books</option>
-              <option value='11'>Film</option>
-              <option value='12'>Music</option>
-              <option value='13'>Musicals &amp; Theatres</option>
-              <option value='14'>Television</option>
-              <option value='15'>Video Games</option>
-              <option value='16'>Board Games</option>
-              <option value='29'>Comics</option>
-              <option value='17'>Science &amp; Nature</option>
-              <option value='18'>Computers</option>
-              <option value='19'>Mathematics</option>
-              <option value='20'>Mythology</option>
-              <option value='21'>Sports</option>
-              <option value='22'>Geography</option>
-              <option value='23'>History</option>
-              <option value='24'>Politics</option>
-              <option value='25'>Art</option>
-              <option value='26'>Celebrities</option>
-              <option value='27'>Animals</option>
-              <option value='28'>Vehicles</option>
-              <option value='30'>Gadgets</option>
-              <option value='31'>Japanese Anime &amp; Manga</option>
-              <option value='32'>Cartoon &amp; Animations</option>
-            </select>
+            <button onClick={getNewTrivia}>Play</button>
+            <Categories category={category} setCategory={setCategory} />
+
             {userAnswers.length > 0 && (
               <button
                 className='show-button'
